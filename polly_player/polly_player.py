9065732d4c0265d6b8196b8fcb2b338902b7453e
Christian Eriksson <christian.eriksson@dewire.com>
2018-11-10 00:18:46 +0100

    [TASK] Added Polly

--- polly_player/polly_player.py
+++ polly_player/polly_player.py
@@ -1,16 +1,26 @@
 import boto3
+from contextlib import closing
+import subprocess
+import sys
+
+text=str(sys.argv[1])
 
 client = boto3.client('polly', 'eu-central-1')
 
 response = client.synthesize_speech(
     OutputFormat='mp3',
-    Text='Hello World',
-    VoiceId='Carmen'
+    Text=text,
+    VoiceId='Joanna'
 )
+
+file_name="polly_audio.mp3"
+padded_file="polly_audio_padded.mp3"
 
 if "AudioStream" in response:
     with closing(response["AudioStream"]) as stream:
         data = stream.read()
-        fo = open("pollytest.mp3", "w+")
+        fo = open(file_name, "w+")
         fo.write( data )
         fo.close()
+    subprocess.call("sox " + file_name + " -r 44100 -c 2 " + padded_file + " pad 1.0 1.0", shell=True)
+    subprocess.call("play " + padded_file, shell=True)
